169525905b8d5fb9782d90ea37cc1a1c41137cee
John Deck <jdeck88@gmail.com>
2016-05-18 16:16:45 -0700

    tweeking names, settings

--- src/main/web/app/app.constants.js
+++ src/main/web/app/app.constants.js
@@ -1,6 +1,6 @@
 angular.module('armsApp')
 
     .constant("PROJECT_ID", 30)
-    .constant("REST_ROOT", "/arms-fims/rest/")
+    .constant("REST_ROOT", "/arms/rest/")
     // When changing this, also need to change <base> tag in index.html
-    .constant("APP_ROOT", "/arms-fims/");
+    .constant("APP_ROOT", "/arms/");
